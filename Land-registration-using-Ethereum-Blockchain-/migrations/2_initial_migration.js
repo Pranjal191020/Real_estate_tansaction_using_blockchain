09f68978271d86c50002e5feac31b7bb1b3e9eda
Pranjal Munot <80443146+Pranjal191020@users.noreply.github.com>
2023-02-28 13:05:56 +0530

    Add files via upload

---
+++ Land-registration-using-Ethereum-Blockchain-/migrations/2_initial_migration.js
@@ -0,0 +1,4 @@
+var Migrations = artifacts.require("C:\Users\hp\Land-registration-using-Ethereum-Blockchain-\contracts\Structregistry.sol");
+module.exports = function(deployer) {
+  deployer.deploy(Migrations);
+};
