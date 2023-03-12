815dd6e244f5b51f6fbf0799068140c7584a7b3b
Pranjal Munot <80443146+Pranjal191020@users.noreply.github.com>
2023-03-12 21:11:55 +0530

    Delete Land-registration-using-Ethereum-Blockchain- directory

--- Land-registration-using-Ethereum-Blockchain-/migrations/1_initial_migration.js
+++
@@ -1,5 +0,0 @@
-var Migrations = artifacts.require("C:\Users\hp\Land-registration-using-Ethereum-Blockchain-\contracts\Migrations.sol");
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-};
-
--- Land-registration-using-Ethereum-Blockchain-/migrations/2_initial_migration.js
+++
@@ -1,4 +0,0 @@
-var Migrations = artifacts.require("C:\Users\hp\Land-registration-using-Ethereum-Blockchain-\contracts\Structregistry.sol");
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-};
